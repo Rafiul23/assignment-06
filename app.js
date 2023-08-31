4b31251620cad24184fccdbfdd706f989dabd769
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-01 04:52:41 +0600

    category buttons are made effective

--- app.js
+++ app.js
@@ -3,38 +3,44 @@ const loadCategory = async () =>{
     const data = await res.json();
     const dataArray = data.data;
     dataArray.forEach( el => {
-        const categoryDiv = document.getElementById('category-div');
-        const categoryButton = document.createElement('button');
-        categoryButton.classList = 'btn ml-5 hover:bg-[#FF1F3D] hover:text-white text-black';
-        categoryButton.innerText = el.category;
-        categoryDiv.appendChild(categoryButton);
+        const categoryContainer = document.getElementById('category-container');
+        const categoryDiv = document.createElement('div');
+        
+        categoryDiv.innerHTML = `
+            <button class='btn ml-5 hover:bg-[#FF1F3D] hover:text-white text-black' onclick="loadCards('${el.category_id}')">${el.category}</button>
+        `;
+        categoryContainer.appendChild(categoryDiv);
     });
     
 }
 
-const loadAllCards = async () =>{
-    const res = await fetch ('https://openapi.programming-hero.com/api/videos/category/1000');
+const loadCards = async (id) =>{
+    const res = await fetch (`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
     const cardInfoArray = data.data;
+
     const cardContainer = document.getElementById('card-container');
+    cardContainer.innerText = '';
+
     cardInfoArray.forEach( el =>{
-        const card = document.createElement('div');
-        card.classList = 'card bg-base-100 shadow-lg'
-        card.innerHTML = `
+        const allCard = document.createElement('div');
+        allCard.classList = 'card bg-base-100 shadow-lg'
+        allCard.innerHTML = `
 
         <figure><img class="h-[250px] w-full" src="${el.thumbnail}" /></figure>
         <div class="flex gap-1">
            <img class="w-[80px] mt-5 ml-5 h-[80px] rounded-full" src="${el.authors[0].profile_picture}" />
            <div class='card-body'>
            <h2 class="card-title">${el.title}</h2>
-           <p>${el.authors[0].profile_name} ${el.authors[0].verified ? el.authors[0].verified = '<i class="fa-solid fa-circle-check" style="color: #005eff;"></i>' : ' '}</p>
+           <p>${el.authors[0].profile_name} ${el.authors[0].verified ? el.authors[0].verified = '<i class="fa-solid fa-circle-check ml-1" style="color: #005eff;"></i>' : ' '}</p>
            <p>${el.others.views}</p>
            </div>
         </div>
         `;
-        cardContainer.appendChild(card);
+        cardContainer.appendChild(allCard);
     })
 }
 
+
 loadCategory();
-loadAllCards();
+loadCards(1000);
