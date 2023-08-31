ce791a919d47683c53f66b74d6a383f204263233
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-01 01:12:28 +0600

    an error is resolved

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ const loadCategory = async () =>{
     
 }
 
+const loadAllCards = async () =>{
+    const res = await fetch ('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await res.json();
+    const cardInfoArray = data.data;
+    cardInfoArray.forEach( el =>{
+
+    })
+}
+
 loadCategory();
+loadAllCards();
