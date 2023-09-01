d00fbb306dec1e2f254ea7fec1bdb2d3bcdc0c16
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-02 00:37:36 +0600

    sorted by views for all category is implemented

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+
 const loadCategory = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
     const dataArray = data.data;
+
     dataArray.forEach( el => {
         const categoryContainer = document.getElementById('category-container');
         const categoryDiv = document.createElement('div');
         
         categoryDiv.innerHTML = `
-            <button class='btn ml-5 hover:bg-[#FF1F3D] hover:text-white text-black' onclick="loadCards('${el.category_id}')">${el.category}</button>
+            <button id="${el.category_id}" class='btn ml-5 hover:bg-[#FF1F3D] hover:text-white text-black' onclick="loadCards('${el.category_id}')">${el.category}</button>
         `;
         categoryContainer.appendChild(categoryDiv);
     });
-    
+   
 }
+
+
 
 const loadCards = async (id) =>{
     const res = await fetch (`https://openapi.programming-hero.com/api/videos/category/${id}`);
@@ -27,7 +31,7 @@ const loadCards = async (id) =>{
     if(arrayLength > 0){
         cardInfoArray.forEach( el =>{
             const allCard = document.createElement('div');
-            allCard.classList = 'card bg-base-100 shadow-lg'
+            allCard.classList = 'card w-full bg-base-100 shadow-lg'
     
             const getTimeString = el.others.posted_date;
             const getTime = parseInt(getTimeString);
@@ -37,14 +41,14 @@ const loadCards = async (id) =>{
             const minutes = Math.floor((getTime % 3600) / 60);
             const seconds = ((getTime % 3600) % 60);
     
-            console.log(hours, minutes, seconds);
+           
     
             allCard.innerHTML = `
     
            <div class='relative'>
            <figure><img class="h-[250px] w-full" src="${el.thumbnail}" /></figure>
              <div class='absolute p-2 rounded-lg bottom-1 right-2 bg-black text-white'>
-                ${(!isNaN(getTime) ? (hours + 'hrs ' + minutes + ' min ' + seconds + 's ago') : ' '  )}
+                ${(!isNaN(getTime) ? (hours + 'hrs ' + minutes + ' min ago') : ' '  )}
              </div>
            </div>
             <div class="flex gap-1">
@@ -71,8 +75,58 @@ const loadCards = async (id) =>{
         `;
 
     }
+    
 }
-
 
 loadCategory();
 loadCards(1000);
+
+const sortByView = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+
+    const data = await res.json();
+    const dataArray = data.data;
+
+    const container = document.getElementById('card-container');
+    container.innerText = '';
+
+    dataArray.sort((a,b) =>{
+       return (parseFloat(b.others.views.slice(0, -1)) - parseFloat(a.others.views.slice(0, -1)));
+    });
+
+    // console.log(dataArray);
+
+    dataArray.forEach( el =>{
+        const allCard = document.createElement('div');
+        allCard.classList = 'card w-full bg-base-100 shadow-lg'
+
+        const getTimeString = el.others.posted_date;
+        const getTime = parseInt(getTimeString);
+        
+
+        const hours = Math.floor((getTime / 3600));
+        const minutes = Math.floor((getTime % 3600) / 60);
+        const seconds = ((getTime % 3600) % 60);
+
+        allCard.innerHTML = `
+
+       <div class='relative'>
+       <figure><img class="h-[250px] w-full" src="${el.thumbnail}" /></figure>
+         <div class='absolute p-2 rounded-lg bottom-1 right-2 bg-black text-white'>
+            ${(!isNaN(getTime) ? (hours + 'hrs ' + minutes + ' min ago') : ' '  )}
+         </div>
+       </div>
+        <div class="flex gap-1">
+           <img class="w-[80px] mt-5 ml-5 h-[80px] rounded-full" src="${el.authors[0].profile_picture}" />
+           <div class='card-body'>
+           <h2 class="card-title">${el.title}</h2>
+           <p>${el.authors[0].profile_name} ${el.authors[0].verified ? el.authors[0].verified = '<i class="fa-solid fa-circle-check ml-1" style="color: #005eff;"></i>' : ' '}</p>
+           <p>${el.others.views}</p>
+           </div>
+        </div>
+        `;
+        container.appendChild(allCard);
+    }
+    )
+}
+
