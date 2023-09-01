1b360d99647641f1f2829dc0fa15a3815e86dde1
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-01 07:52:44 +0600

    warning page is loaded and showing warning correctly"

--- app.js
+++ app.js
@@ -20,41 +20,57 @@ const loadCards = async (id) =>{
     const cardInfoArray = data.data;
 
     const cardContainer = document.getElementById('card-container');
-    cardContainer.innerText = '';
+    cardContainer.innerHTML = '';
 
-    cardInfoArray.forEach( el =>{
-        const allCard = document.createElement('div');
-        allCard.classList = 'card bg-base-100 shadow-lg'
+    const arrayLength = cardInfoArray.length;
 
-        const getTimeString = el.others.posted_date;
-        const getTime = parseInt(getTimeString);
-        
-
-        const hours = Math.floor((getTime / 3600));
-        const minutes = Math.floor((getTime % 3600) / 60);
-        const seconds = ((getTime % 3600) % 60);
-
-        console.log(hours, minutes, seconds);
-
-        allCard.innerHTML = `
-
-       <div class='relative'>
-       <figure><img class="h-[250px] w-full" src="${el.thumbnail}" /></figure>
-         <div class='absolute p-1 bottom-1 right-2 bg-black text-white'>
-            ${(!isNaN(getTime) ? (hours + ' : ' + minutes + ' : ' + seconds) : ' '  )}
-         </div>
-       </div>
-        <div class="flex gap-1">
-           <img class="w-[80px] mt-5 ml-5 h-[80px] rounded-full" src="${el.authors[0].profile_picture}" />
-           <div class='card-body'>
-           <h2 class="card-title">${el.title}</h2>
-           <p>${el.authors[0].profile_name} ${el.authors[0].verified ? el.authors[0].verified = '<i class="fa-solid fa-circle-check ml-1" style="color: #005eff;"></i>' : ' '}</p>
-           <p>${el.others.views}</p>
+    if(arrayLength > 0){
+        cardInfoArray.forEach( el =>{
+            const allCard = document.createElement('div');
+            allCard.classList = 'card bg-base-100 shadow-lg'
+    
+            const getTimeString = el.others.posted_date;
+            const getTime = parseInt(getTimeString);
+            
+    
+            const hours = Math.floor((getTime / 3600));
+            const minutes = Math.floor((getTime % 3600) / 60);
+            const seconds = ((getTime % 3600) % 60);
+    
+            console.log(hours, minutes, seconds);
+    
+            allCard.innerHTML = `
+    
+           <div class='relative'>
+           <figure><img class="h-[250px] w-full" src="${el.thumbnail}" /></figure>
+             <div class='absolute p-2 rounded-lg bottom-1 right-2 bg-black text-white'>
+                ${(!isNaN(getTime) ? (hours + 'hrs ' + minutes + ' min ' + seconds + 's ago') : ' '  )}
+             </div>
            </div>
-        </div>
+            <div class="flex gap-1">
+               <img class="w-[80px] mt-5 ml-5 h-[80px] rounded-full" src="${el.authors[0].profile_picture}" />
+               <div class='card-body'>
+               <h2 class="card-title">${el.title}</h2>
+               <p>${el.authors[0].profile_name} ${el.authors[0].verified ? el.authors[0].verified = '<i class="fa-solid fa-circle-check ml-1" style="color: #005eff;"></i>' : ' '}</p>
+               <p>${el.others.views}</p>
+               </div>
+            </div>
+            `;
+            cardContainer.appendChild(allCard);
+            const warningContainer = document.getElementById('warning');
+            warningContainer.innerText = '';
+        })
+    } else {
+        cardContainer.innerHTML = '';
+        const warningContainer = document.getElementById('warning');
+        warningContainer.innerHTML = `
+        
+        <img class="mx-auto" src='./images/Icon.png' />
+        <p class="font-bold mt-5 text-2xl">Oops!! Sorry, There is no content here</p>
+        
         `;
-        cardContainer.appendChild(allCard);
-    })
+
+    }
 }
 
 
